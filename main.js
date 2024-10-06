3d9503099e8ba05978abfd3a6255d272cd1758aa
papeze <pasi.j.rautiainen@outlook.com>
2024-10-06 14:13:54 +0300

    Base files for edit

---
+++ main.js
@@ -0,0 +1,55 @@
+"use strict";
+"@ts-check"
+
+// TIEA207
+
+// Tapahtuman kuuntelija sivun lataamiselle
+window.addEventListener("load", function() {
+
+    // Hakee datan lähteestä
+    let getdata = async function() {
+        const response = await fetch('lähde', {"credentials": "include"});
+        //data = await response.json();
+        return await response.text();
+    }
+
+    let data = getdata;
+    document.addEventListener(
+        "keydown",
+        // Kuuntelija näppäimen painamiselle
+        function keyPressed(event) {
+          if (event.defaultPrevented) {
+            return; // Do nothing if the event was already processed
+          }
+          // Näppäin jota on painettu
+          const key = event.key;
+          playByKey(key, data);
+
+          // tämä uuteen tiedostoon
+          // funktiossa voisi olla taulukko näppäimistä
+          // ja niihin yhdistää ääni silmukassa
+        function playByKey(key, data) {
+            let keys = [a,b,c];
+            // taulukko myös nappula elementeistä
+            let buttons = [document.getElementById("nappula1"),document.getElementById("nappula1")];
+
+            // myös data pitää jotekin erotella.
+            // sitten yhdistää näppäimeen tässä silmukassa
+            for(let v in keys) {
+                v == key;
+                if(v == "Ääni") {
+                    // soita ääni, esim.
+                    document.getElementById("Audio").play();
+                    // tai
+                    const audio = new Audio('path/to/your/soundfile.mp3');
+                    // Play the audio
+                    audio.play();
+                    break;
+                }
+            }
+        }
+
+        });
+
+
+});
