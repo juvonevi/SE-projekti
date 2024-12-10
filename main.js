8208f8e55743a976b5569cdb42e83bc7d42896af
Viianen Noe <noe.n.viianen@student.jyu.fi>
2024-12-10 14:38:59 +0200

    Lisätty teemoja, sovellus muistaa käyttäjän valitseman teeman

--- main.js
+++ main.js
@@ -23,10 +23,10 @@ window.addEventListener("load", function() {
     const defaultKeys2 = ["q","w","e","r","t","y","u","i","o","p"];  
     let myKeys = Array.from(defaultKeys);
     let myKeys2 = Array.from(defaultKeys2);
-    let themes = ["lightmode", "darkmode.css", "lightpink.css", "darkpink.css", "red.css", "darkgreen.css"]; //css tiedoston nimi
-    let themeIcons = ["☼", "☾", "☆", "★", "☂", "☘"]; //Napin käyttämä ikoni
-    let themeColors = ["lightblue", "#3d414a", "#ffb3d9", "#b3005ad3", "#b22222", "#637c36"]; //Napin käyttämä väri (css-tiedoston second color)
-    let themeTextColors = ["#011a42", "white", "#1a000d", "white", "white", "white"]; //Napin tekstin väri (css-tiedoston text color)
+    let themes = ["lightmode", "darkmode.css", "lightpink.css", "darkpink.css", "red.css", "darkgreen.css", "lightgreen.css", "darkblue.css", "beige.css"]; //css tiedoston nimi
+    let themeIcons = ["☼", "☾", "☆", "★", "♥", "☘", "✿", "☁", "⯌"]; //Napin käyttämä ikoni
+    let themeColors = ["lightblue", "#3d414a", "#ffb3d9", "#b3005ad3", "#b22222", "#637c36", "#8db969", "#303858", "#b89a81"]; //Napin käyttämä väri (css-tiedoston second color)
+    let themeTextColors = ["#011a42", "white", "#1a000d", "white", "white", "white", "rgb(22, 44, 28)", "white", "rgb(43, 29, 24)"]; //Napin tekstin väri (css-tiedoston text color)
 
     themes.chosen = 0;
 
@@ -209,8 +209,14 @@ window.addEventListener("load", function() {
      * Vaihtaa teeman
      * @param {*} e 
      */
-    function changeTheme(e) {
-        let theme = e.target.theme;
+    function changeTheme(e, themeNumber) {
+        let theme;
+        if (e) {
+            theme = e.target.theme;
+        }
+        else {
+            theme = themeNumber;
+        }
         let links = document.getElementsByTagName("link");
         for (let link of links) {
             if (link.href.includes(themes[themes.chosen])) {
@@ -227,6 +233,7 @@ window.addEventListener("load", function() {
             firstLink.after(link);
         }
         themes.chosen = theme;
+        localStorage.setItem("chosenTheme", theme);
         document.getElementById("chooseTheme").style.display = "none";
         changeThemeOpen = false;
     }
@@ -310,6 +317,16 @@ window.addEventListener("load", function() {
         }
     };
     applyDarkMode(isDarkMode());
+
+    changeToUserChosenTheme();
+    /**
+     * Vaihtaa teeman käyttäjän viimeksi valitsemaan teemaan
+     */
+    function changeToUserChosenTheme() {
+        if (localStorage.getItem("chosenTheme")) {
+            changeTheme(null, parseInt(localStorage.getItem("chosenTheme")));
+        }
+    }
 
     /**
      * Hakee käyttäjän äänet local storagesta
